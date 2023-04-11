7bd2bfbfa52bd8f3d30fcadea0f0b9332772e166
Hengameh M.Khani <afsooneday@gmail.com>
2023-04-11 20:53:36 +0100

    subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,29 +1,4 @@
- import React from 'react'
-// import "./Subtotal.css";
-// import {useStateValue} from"./StateProvider";
-// import {getBasketTotal} from "./reducer";
-
-
-// function Subtotal() {
-//   const[{basket}] = useStateValue();
-//   const  subtotal = getBasketTotal(basket);
-//   return(
-//     <div className='subtotal'>
-//       <p>
-//         subtotal ({basket?.lenght} items):<strong>${subtotal}</strong>
-//       </p>
-//       <small className='subtotal__gift'>
-//         <input type ="checkbox" /> This order contains a gift
-
-//       </small>
-//       <button>Proceed to checkout</button>
-
-//     </div>
-//   );
-  
-// }
- 
-// export default Subtotal
+ import React from 'react'    
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 
